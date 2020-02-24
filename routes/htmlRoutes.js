aea648365eda80fb5284a8b374f17aff1a77d31d
Davey Wong <notwdjw@gmail.com>
2020-02-23 18:31:03 -0800

    Article scrape added using axios and cheerio

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,28 @@
+var cheerio = require('cheerio')
+var axios = require('axios')
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index')
+    });
+
+    app.get('/scraped', function (req, res) {
+        axios.get("https://www.wsj.com/").then(function (response) {
+            var $ = cheerio.load(response.data);
+
+            $('article div').each(function (i, element) {
+                var result = {};
+
+                console.log($(this).text());
+                console.log($(this).find('a').attr('href'));
+                
+                
+
+
+
+
+            })
+
+
+        })
     });
 };
